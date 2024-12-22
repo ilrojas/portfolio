0cb6db8991cb722e7e8bb181dabe1ac936b163f1
ilrojas <ivan88rodriguezrojas@gmail.com>
2024-12-22 14:18:46 -0300

    design changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,9 +14,25 @@ export default {
 					opacity: '1',
 				  },
 				},
+				spinSlow: {
+					'0%': { transform: 'rotate(0deg)' },
+					'100%': { transform: 'rotate(360deg)' },
+				},
+				fly:{
+					from: {
+						transform: 'translate3d(0, 0, 0)',
+						opacity: '1',
+					  },
+					  to: {
+						transform: 'translate3d(0, -80%, 0)',
+						opacity: '0',
+					  },
+				}
 			  },
 			  animation: {
 				moveDown: 'moveDown .9s ease-in-out',
+				spinSlow: 'spinSlow 18s linear infinite',
+				fly: 'fly 2s linear infinite'
 			  }
 		},
 	},
