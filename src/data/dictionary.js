a52d26dac8043440c0cb6d0fc2af047a0de3ba60
ilrojas <ivan88rodriguezrojas@gmail.com>
2025-01-10 03:17:21 -0300

    adding table of skills and score

--- src/data/dictionary.js
+++ src/data/dictionary.js
@@ -1,5 +1,5 @@
 export const dictionary = {
-    'English': {
+    en: {
         welcome: "Welcome to my portfolio",
         about: "About Me",
         projects: "Projects",
@@ -12,10 +12,12 @@ export const dictionary = {
         theme: "Change Theme",
         slogan: "I'm a Frontend Developer",
         created: "Created by",
-        description: "I am a front-end developer with experience in creating modern and functional interfaces using technologies like React, Tailwind CSS, and Astro. I am passionate about turning ideas into intuitive and visually appealing web experiences."
+        description: "I am a front-end developer with experience in creating modern and functional interfaces using technologies like React, Tailwind CSS, and Astro. I am passionate about turning ideas into intuitive and visually appealing web experiences.",
+        technical: "Technical Skills",
+        soft: "Others",
         // Add more key-value pairs as needed
     },
-    'Español': {
+    es: {
         welcome: "Bienvenido a mi portafolio",
         home: "Inicio",
         about: "Sobre mí",
@@ -29,7 +31,9 @@ export const dictionary = {
         theme: "Cambiar Tema",
         slogan: "Desarrollador Frontend",
         created: "Creado por",
-        description: "Soy un desarrollador front-end con experiencia en la creación de interfaces modernas y funcionales utilizando tecnologías como React, Tailwind CSS y Astro. Me apasiona convertir ideas en experiencias web intuitivas y visualmente atractivas."
+        description: "Soy un desarrollador front-end con experiencia en la creación de interfaces modernas y funcionales utilizando tecnologías como React, Tailwind CSS y Astro. Me apasiona convertir ideas en experiencias web intuitivas y visualmente atractivas.",
+        technical: "Habilidades Técnicas",
+        soft: "Otras",
         // Add more key-value pairs as needed
     }
 };
---
+++ src/data/skills.js
@@ -0,0 +1,180 @@
+export const skills = {
+  technical: [
+    {
+      name: "HTML",
+      icon: "",
+      rating: 9,
+      color: "#FF5733",
+    },
+    {
+      name: "CSS",
+      icon: "",
+      rating: 8,
+      color: "#FFBD33",
+    },
+    {
+      name: "JavaScript",
+      icon: "",
+      rating: 8,
+      color: "#DBFF33",
+    },
+    {
+      name: "TypeScript",
+      icon: "",
+      rating: 8,
+      color: "#75FF33",
+    },
+    {
+      name: "Tailwind CSS",
+      icon: "",
+      rating: 10,
+      color: "#33FF57",
+    },
+    {
+      name: "Git/GitHub",
+      icon: "",
+      rating: 7,
+      color: "#33FFBD",
+    },
+    {
+      name: "React",
+      icon: "",
+      rating: 7,
+      color: "#33DBFF",
+    },
+    {
+      name: "Astro",
+      icon: "",
+      rating: 8,
+      color: "#3375FF",
+    },
+    {
+      name: "Java",
+      icon: "",
+      rating: 6,
+      color: "#5733FF",
+    },
+    {
+      name: "Spring Boot",
+      icon: "",
+      rating: 6,
+      color: "#BD33FF",
+    },
+  ],
+  soft: [
+    {
+      es: "Español",
+      en: "Spanish",
+      icon: "",
+      rating: 10,
+      color: "#FF6F61",
+    },
+    {
+      es: "Ingles",
+      en: "English",
+      icon: "",
+      rating: 7,
+      color: "#6B5B95",
+    },
+    {
+      es: "Comunicación",
+      en: "Communication",
+      icon: "",
+      rating: 10,
+      color: "#88B04B",
+    },
+    {
+      es: "Trabajo en equipo",
+      en: "Teamwork",
+      icon: "",
+      rating: 10,
+      color: "#F7CAC9",
+    },
+    {
+      es: "Resolución de problemas",
+      en: "Problem solving",
+      icon: "",
+      rating: 10,
+      color: "#92A8D1",
+    },
+    {
+      es: "Atención al detalle",
+      en: "Attention to detail",
+      icon: "",
+      rating: 9,
+      color: "#955251",
+    },
+    {
+      es: "Gestión del tiempo",
+      en: "Time management",
+      icon: "",
+      rating: 10,
+      color: "#B565A7",
+    },
+  ],
+};
+
+const technicalColors = [
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#FF33DB",
+  "#FF3375",
+];
+
+const softColors = [
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#",
+  "#009B77",
+  "#DD4124",
+  "#D65076",
+  "#45B8AC",
+  "#EFC050",
+  "#5B5EA6",
+  "#9B2335",
+  "#DFCFBE",
+  "#55B4B0",
+  "#E15D44",
+  "#7FCDCD",
+  "#BC243C",
+  "#C3447A",
+  "#98B4D4",
+  "#FF6F61",
+  "#6B5B95",
+  "#88B04B",
+  "#F7CAC9",
+  "#92A8D1",
+  "#955251",
+  "#B565A7",
+  "#009B77",
+  "#DD4124",
+  "#D65076",
+  "#45B8AC",
+];
+
+/* let technicalIndex = 0;
+let softIndex = 0;
+
+for (const key in skills.technical) {
+    skills.technical[key].rating = Math.floor(Math.random() * 5) + 1;
+    skills.technical[key].color = technicalColors[technicalIndex % technicalColors.length];
+    technicalIndex++;
+}
+
+for (const key in skills.soft) {
+    skills.soft[key].rating = Math.floor(Math.random() * 5) + 1;
+    skills.soft[key].color = softColors[softIndex % softColors.length];
+    softIndex++;
+} */
