c940b59791a11cb79781f23bb7c69f3b4df0193b
ilrojas <ivan88rodriguezrojas@gmail.com>
2025-02-09 03:22:33 -0300

    adding animations to skills sections

--- src/content/education/university.md
+++ src/content/education/university.md
@@ -4,4 +4,4 @@ period: 2007-2012
 title: Ingeniero en Ciencias Informáticas
 location: Havana, Cuba
 ---
-Aqui va el contenido o descripcion de mi tiempo en Univesidad de las Ciencias Informáticas
+Curso 5 años en la Universidad de las Ciencias Informáticas graduándome en el año 2012, con el título de Ingeniero en Ciencias Informáticas. Obteniendo conocimientos de Ingeniería y Desarrollo de Software. Especializándome en el desarrollo web  y siendo mi especialidad hasta el dia de hoy. Incorporando continuamente a mis conocimientos las nuevas tecnologías.
--- src/data/iconCoder.js
+++ src/data/iconCoder.js
@@ -1,4 +1,4 @@
-export const iconCoder = `<section class="group text-center flex flex-row">
+export const iconCoder = `<section class="cursor-pointer group text-center flex flex-row font-sans">
 <span class="text-4xl sm:text-4xl md:text-3xl lg:text-4xl">{</span>
 <div class=" relative text-2xl sm:text-2xl md:text-3xl lg:text-2xl flex flex-col space-y-0 align-middle">
     <div class="transition-opacity duration-300  opacity-100 group-hover:opacity-0">De</div>
@@ -8,10 +8,23 @@ export const iconCoder = `<section class="group text-center flex flex-row">
 </div>
 <span class="text-4xl sm:text-4xl md:text-3xl lg:text-4xl">}</span><sup class="-rotate-45">v</sup>
 <span class="relative  text-3xl   font-extrabold z-10">
-  IRCodify
+  <span class="text-[#A8FF78]">IR</span><span class="">Codify</span>
 </span>
 
 
 </section>`;
 /* Òó */
 const iconCoder1 = "{òÓ,}";
+
+export const iconCoder2 = `<section class="text-[#63B3ED] cursor-pointer group text-center flex flex-row font-sans">
+<span class="text-4xl sm:text-4xl md:text-3xl lg:text-4xl">{</span>
+<div class=" relative text-2xl sm:text-2xl md:text-3xl lg:text-2xl flex flex-col space-y-0 align-middle">
+    ->
+</div>
+<span class="text-4xl sm:text-4xl md:text-3xl lg:text-4xl">}</span>
+<span class="relative  text-3xl   font-extrabold z-10">
+  <span class="text-[#A8FF78]">IR</span><span class="">Codify</span>
+</span>
+
+
+</section>`;
