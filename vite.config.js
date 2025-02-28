cc393e7b61080fcc5d5355ef2cd34503600b07ea
ilrojas <ivan88rodriguezrojas@gmail.com>
2025-02-28 10:28:29 -0300

    appliying changes to layouts

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+/* import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    host: '127.0.0.1',
+    port: 4321,
+    strictPort: true,
+    allowedHosts: ['*'], // Permitir cualquier host
+    cors: true, // Habilitar CORS
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Permitir solicitudes externas
+    },
+  },
+}); */
+
